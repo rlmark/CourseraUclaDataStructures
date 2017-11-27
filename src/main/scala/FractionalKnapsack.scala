701d21253bd05e251e9a5c180740f02fca2cc3de
Rebecca Mark <rebecca.lmark@gmail.com>
2017-11-27 00:52:16 -0800

    Simplified recursive function, fixed stupid rounding errors

--- src/main/scala/FractionalKnapsack.scala
+++ src/main/scala/FractionalKnapsack.scala
@@ -5,42 +5,47 @@ object FractionalKnapsack {
   def getOptimalValue(capacity: Int, values: Seq[Int], weights: Seq[Int]): Double ={
     // Take the values and weights and sort by the higest value per weight pair.
     val tuples: Seq[ValueAndWeight] = values.zip(weights)
-      val sorted = tuples.sortWith(sortTuples)
+    val sorted = tuples.sortWith(sortTuples)
 
     def loop(capacityLeft: Int, potentialLootLeft: Seq[(Int, Int)], currentLootValue: Double): Double = {
-      if(capacityLeft == 0) return currentLootValue
-      else {
+//      if(capacityLeft == 0) currentLootValue
+//      else {
         potentialLootLeft match {
-          case (v, w)::tail => {
+          case (v, w)::tail =>
             // if your remaining capacity is greater than the weight of loot available
-            if (capacityLeft > w) {
+            if (capacityLeft >= w) {
               val newLootValue = v + currentLootValue // add the entire value of the loot to current val
               val newRemainingCapacity = capacityLeft - w // subtract the entire weight of the loot from the current capacity
               loop(newRemainingCapacity, tail, newLootValue) // recursive call
             }
             // your remaining capacity is less than the weight you can put in your knapsack
-            // so you need fractional value returned, no recursing
+            // so you need fractional value returned, no recursive call
             else {
               // value per weight
-              val valuePerWeight = v.toDouble/w
-              BigDecimal(valuePerWeight * capacityLeft + currentLootValue).setScale(4, BigDecimal.RoundingMode.HALF_UP).toDouble
-
+              val valuePerWeight: BigDecimal = BigDecimal.valueOf(v.toDouble/w)
               // shouldn't need to recurse
               // must have 4 digits of precision
+              (valuePerWeight * capacityLeft + currentLootValue).setScale(4, BigDecimal.RoundingMode.HALF_UP).toDouble
             }
-          }
           case Nil => currentLootValue // your knapsack is bigger than available loot
         }
       }
-    }
+//    }
 
-    loop(capacity, sorted, 0.0)
+   loop(capacity, sorted, 0.0)
   }
 
   def sortTuples(t1: (ValueAndWeight), t2: (ValueAndWeight)): Boolean = {
-    t1._1 / t1._2 > t2._1 / t2._2
+    BigDecimal.valueOf(t1._1.toDouble / t1._2).setScale(10, BigDecimal.RoundingMode.HALF_UP) > BigDecimal.valueOf(t2._1.toDouble / t2._2).setScale(10, BigDecimal.RoundingMode.HALF_UP)
   }
 
+
+  def getOptimalValue2(capacity: Int, values: Seq[Int], weights: Seq[Int]): Double ={
+    for (i <- values.indices) {
+
+    }
+    ???
+  }
   def main(args: Array[String]): Unit = {
     val scanner = new Scanner(System.in)
     val n = scanner.nextInt()
--- src/test/scala/FractionalKnapsackSpec.scala
+++ src/test/scala/FractionalKnapsackSpec.scala
@@ -3,18 +3,27 @@ import org.scalatest.{Matchers, FlatSpec}
 class FractionalKnapsackSpec extends FlatSpec with Matchers {
   "FractionalKnapsack" should "pack highest value" in {
     val result = FractionalKnapsack.getOptimalValue(3,Seq(5, 10, 1), Seq(1,1,1))
-    result shouldBe ()
+    result shouldBe (16)
   }
   it should "work" in {
-
-
     val result = FractionalKnapsack.getOptimalValue(50,Seq(60, 100, 120), Seq(20,50,30))
     result shouldBe 180.0
 
   }
-  it should "work for fracitonal amounts" in {
-
+  it should "work for fractional amounts" in {
     val result = FractionalKnapsack.getOptimalValue(10,Seq(500), Seq(30))
     result shouldBe 166.6667
+  }
+  it should "work when capacity left is 0" in {
+    val result = FractionalKnapsack.getOptimalValue(1,Seq(500), Seq(1))
+    result shouldBe 500.0
+  }
+  it should "work for another ex" in {
+    val result = FractionalKnapsack.getOptimalValue(50,Seq(60, 100, 120), Seq(10, 20, 30))
+    result shouldBe 240.0
+  }
+  it should "work when same value ratio elements are given" in {
+    val result = FractionalKnapsack.getOptimalValue(3,Seq(5, 10, 1), Seq(5,10,1))
+    result shouldBe (3)
   }
 }
