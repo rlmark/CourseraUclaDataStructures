83176dac4f7e5b313f1c788eec4bb5d490c25f3d
Rebecca Mark <rebecca.lmark@gmail.com>
2017-11-28 22:23:49 -0800

    Naive implementation of Fibonacci Huge mod N

--- src/main/scala/FibonacciHuge.scala
+++ src/main/scala/FibonacciHuge.scala
@@ -13,9 +13,58 @@ object FibonacciHuge {
       current = previous + current_temp
 
       i += 1
-
     }
     current % mod
+  }
+
+  def calcFib(n: Long): Long = {
+    def loop(l: Long, r: Long, levelTracker: Int): Long = {
+      if (levelTracker >= n) {
+        r
+      }
+      else loop(r, l + r, levelTracker + 1)
+    }
+
+    if (n == 0) 0
+    else {
+      loop(0, 1, 1)
+    }
+  }
+
+  def findPeriodNaive(mod: Long): Int = {
+    var i = 2 // because period starts with 0 1
+    var notFound = true
+
+    while(notFound) {
+      val currentLast = calcFib(i) % mod
+      val secondLast = calcFib(i - 1) % mod
+
+        if(secondLast == 0 && currentLast == 1){
+          notFound = false
+        }
+
+      i += 1
+    }
+
+  i - 2
+  }
+
+  // can find the period by using only remainders
+//  def findPeriod2(mod: Long): Int = {
+//    var i = 2 // because period starts with 0 1
+//    var notFound = true
+//
+//  }
+
+  def getFibHuge(n: Long, mod: Long): Long = {
+    // Find the period of the modulo level
+    val period = findPeriodNaive(mod)
+
+    // Take n and mod by the length of the period
+    val remainder = n % period
+
+    // Take the remainder of that and mod by original m
+    remainder % mod
   }
 
   def main(args: Array[String]): Unit = {
--- src/test/scala/FibonacciHugeSpec.scala
+++ src/test/scala/FibonacciHugeSpec.scala
@@ -1,6 +1,6 @@
-import org.scalatest.FlatSpec
+import org.scalatest.{Matchers, FlatSpec}
 
-class FibonacciHugeSpec extends FlatSpec {
+class FibonacciHugeSpec extends FlatSpec with Matchers{
   "FibHuge" should "calculate large fib numbers modulo a given number naively" in {
     val result = FibonacciHuge.getFibHugeNaive(5, 3)
     assert(result == 2)
@@ -17,5 +17,34 @@ class FibonacciHugeSpec extends FlatSpec {
     for(i <- 1 to 10){
       print(FibonacciHuge.getFibHugeNaive(i, 21) + " ")
     }
+  }
+  it should "find period in naive manner  length for mod 3" in {
+    // 0 1 1 2 0 2 2 1
+    FibonacciHuge.findPeriodNaive(3) shouldBe 8
+  }
+  it should "find period in naive manner  length for mod 2" in {
+    // 0 1 1
+    FibonacciHuge.findPeriodNaive(2) shouldBe 3
+  }
+  it should "find period in naive manner  length for mod 4" in {
+    FibonacciHuge.findPeriodNaive(4) shouldBe 6
+  }
+  it should "find period in naive manner  length for mod 7" in {
+    FibonacciHuge.findPeriodNaive(7) shouldBe 16
+  }
+  it should "find period in naive manner  length for large mod" in {
+    FibonacciHuge.findPeriodNaive(1000) shouldBe 6
+  }
+  it should "work for non trivial inputs" in {
+    val result = FibonacciHuge.getFibHuge(2015,3)
+    result shouldBe 1
+  }
+  it should "work for small numbers" in {
+    val r = FibonacciHuge.getFibHuge(1, 5)
+    r shouldBe 1
+  }
+  it should "work for long numbers" in {
+    val result = FibonacciHuge.getFibHuge(239, 1000)
+    result shouldBe 161
   }
 }
